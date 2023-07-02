e0f83df9bc7b9a63ab8ec2a043667cf6e93cafb3
SupeerSanny <ksanny556@gmail.com>
2023-07-02 22:10:15 +0530

    GFG & Leetcode POTD (02-07-2023)

--- src/Copy_Set_Bits_in_Range.java
+++ src/Copy_Set_Bits_in_Range.java
@@ -7,11 +7,21 @@ public class Copy_Set_Bits_in_Range {
         int y = sc.nextInt();
         int l = sc.nextInt();
         int r = sc.nextInt();
-        System.out.println(setSetBit(x, y, l, r));
+        System.out.println(setSetBit1(x, y, l, r));
     }
 
     private static int setSetBit(int x, int y, int l, int r) {
         int mask = (int)((-1L << (l-1)) ^ (-1L << r));
         return x | (y & mask);
+    }
+    // Another approach
+    private static int setSetBit1(int x, int y, int l, int r) {
+        int i= l - 1;
+        while(i < r){
+            int rightShift = (y >> i) & 1;
+            x = x | (rightShift << i);
+            i++;
+        }
+        return x;
     }
 }
